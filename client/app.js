eebc190d635a243e5bbb63318fd1f7821a05c2e2
john <redford.john.m@gmail.com>
2015-07-15 12:31:03 -0500

    added command to app.js that requests a websocket connection

---
+++ client/app.js
@@ -0,0 +1,14 @@
+
+//This sends something like
+/*
+GET / HTTP/1.1
+        Host: 127.0.0.1:3000
+        Upgrade: websocket
+        Connection: Upgrade
+        Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==
+        Origin: http://example.com
+        Sec-WebSocket-Protocol: chat, superchat
+        Sec-WebSocket-Version: 13
+*/
+var ws = new WebSocket('ws://127.0.0.1:3000');
+//use with ws.send(JSON.stringify(***))
