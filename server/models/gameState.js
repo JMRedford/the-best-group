bcdfea901f614a2f01bb1e7fa0ac9e043984f110
john <redford.john.m@gmail.com>
2015-07-15 18:22:54 -0500

    added skeleton gameState.js to server

--- server/models/gameState.js
+++ server/models/gameState.js
@@ -1,3 +1,17 @@
 var exports = module.exports = {};
 
 //server side game state storage here
+exports.maxX = 20;  //width of game world in blocks
+exports.maxY = 20;  //height of game world in blocks
+
+exports.players = [];
+
+exports.playerAttacks = [];
+
+exports.enemies = [];
+
+exports.enemyAttacks = [];
+
+exports.init = function(){
+
+}
