7a14b3cec43ba0558908b7d9e9b031c231a9630e
MKS-students <mks.students@hackreactor.com>
2015-07-16 11:55:50 -0500

    Player on screen

---
+++ client/game/components.js
@@ -0,0 +1,62 @@
+
+// An "Actor" is an entity that is drawn in 2D on canvas
+//  via our logical coordinate grid
+// The Grid component allows an element to be located
+//  on a grid of tiles
+Crafty.c('Grid', {
+  init: function() {
+    this.attr({
+      w: Game.map_grid.tile.width,
+      h: Game.map_grid.tile.height
+    })
+  },
+
+  // Locate this entity at the given position on the grid
+  at: function(x, y) {
+    if (x === undefined && y === undefined) {
+      return { x: this.x/Game.map_grid.tile.width, y: this.y/Game.map_grid.tile.height }
+    } else {
+      this.attr({ x: x * Game.map_grid.tile.width, y: y * Game.map_grid.tile.height });
+      return this;
+    }
+  }
+});
+
+
+Crafty.c('Actor', {
+  init: function() {
+    this.requires('2D, Canvas, Grid');
+  },
+});
+
+Crafty.c('PlayerCharacter', {
+  init: function() {
+    this.requires('Actor, Fourway, downWalk1,Collision')
+      .fourway(4)
+      .stopOnSolids()
+      .onHit("Village", this.visitVillage);
+  },
+
+    stopOnSolids: function() {
+    this.onHit('Solid', this.stopMovement);
+
+    return this;
+  },
+  // Stops the movement
+  stopMovement: function() {
+    this._speed = 0;
+    if (this._movement) {
+      this.x -= this._movement.x;
+      this.y -= this._movement.y;
+    }
+  },
+
+});
+
+Crafty.c('Boundary', {
+  init: function() {
+    this.requires('Actor, Color, Solid')
+    .color('rgb(20, 125, 40)');
+  }
+});
+
