7404a3a37edeb719e4fb849c8734b76ac1f671fd
john <redford.john.m@gmail.com>
2015-07-15 20:07:12 -0500

    added distance measurement to game state model

--- server/models/gameState.js
+++ server/models/gameState.js
@@ -15,12 +15,25 @@ exports.playerAttacks = [];
 
 exports.enemyAttacks = [];
 
+var distance = function(loc1,loc2){
+  return Math.pow(Math.pow(loc2[1]-loc1[1],2)+Math.pow(loc2[0]-loc1[0],2),0.5);
+}
+
 exports.init = function(){
 
 }
 
 exports.addPlayer = function(){
-
+  var loc = [Math.random()*exports.maxX*exports.blockSize,
+             Math.random()*exports.maxy*exports.blockSize]
+   do{ 
+    var goodLoc = true;
+    for (var i = 0; i < exports.enemies.length; i++){
+      if (distance(goodLoc,exports.enemies[i].loc) < 50) {
+        goodLoc = false;
+      }
+    }
+  } while (!goodLoc);
 }
 
 exports.addEnemy = function(){
