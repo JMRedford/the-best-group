143f0178e9f16ead5bda736e4f76d4d018e0306a
daniel-jansen <daniel.jansen88@gmail.com>
2015-07-16 12:25:34 -0500

    Implemented static collision detection

--- server/main-server.js
+++ server/main-server.js
@@ -2,19 +2,26 @@ var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
 var path = require('path');
+var gameState = require('./models/gameState.js');
 
 var app = express();
 
 var expressWs = require('express-ws')(app);
 app.use(express.static('../client'));
 
+gameState.init();
+setInterval(gameState.tickTime, 30);
+
 
 /* Route to handle websocket request */
-// app.ws('/', function(sock, req) {
-//   ws.on('message', function(msg) {
-//     //handle a message from the client
-//   });
-// });
+app.ws('/', function(connection, req) {
+  ws.on('message', function(msg) {
+    gameState.message(msg.data.id, msg.data.loc)
+  });
+  setInterval(function(){
+    gameState.sendGameStateToPlayer(connection);
+  },30)
+});
 
 
 var server = app.listen(3000, function() {
@@ -23,3 +30,4 @@ var server = app.listen(3000, function() {
 
   console.log('Server listening on ', port);
 });
+
--- server/models/gameState.js
+++ server/models/gameState.js
@@ -5,8 +5,12 @@ exports.maxX = 20;  //width of game world in blocks
 exports.maxY = 20;  //height of game world in blocks
 
 exports.players = [];
+//player: {'id':#, 'conn':conn, 'loc':loc}
 
 exports.enemies = [];
+
+exports.staticObjects = [];  
+// trees, rocks, etc initialized with loc coords
 
 exports.playerAttacks = [];
 //format of playerAttack: {vector: [origin x, origin y, 
@@ -19,31 +23,96 @@ var distance = function(loc1,loc2){
   return Math.pow(Math.pow(loc2[1]-loc1[1],2)+Math.pow(loc2[0]-loc1[0],2),0.5);
 }
 
-exports.init = function(){
+exports.init = function(enemyAmt){
+  // create enemies
+  for(var i = 0; i < enemyAmt; i++) {
+    exports.addEnemy();
+  }
+};
 
-}
+// implement exports.blockSize
+
+exports.message = function(target_id, target_loc){
+  // search through exports.players array, locate object with matched id, update data
+  for(var i = 0; i < exports.players.length; i++) {
+    if(exports.players[i]['id'] === target_id) {
+      exports.players[i]['loc'] === target_loc;
+    }
+  }  
+};
 
 exports.addPlayer = function(){
+  var newPlayer = {};
   var loc = [Math.random()*exports.maxX*exports.blockSize,
              Math.random()*exports.maxy*exports.blockSize]
    do{ 
     var goodLoc = true;
     for (var i = 0; i < exports.enemies.length; i++){
-      if (distance(goodLoc,exports.enemies[i].loc) < 50) {
+      if (distance(goodLoc, exports.enemies[i].loc) < 50) {
         goodLoc = false;
       }
     }
+    loc = [Math.random()*exports.maxX*exports.blockSize,
+           Math.random()*exports.maxy*exports.blockSize]
   } while (!goodLoc);
-}
+
+  newPlayer['loc'] = loc;
+  exports.players.push(newPlayer);
+};
 
 exports.addEnemy = function(){
+  var newEnemy = {};
+  var loc = [Math.random()*exports.maxX*exports.blockSize,
+             Math.random()*exports.maxy*exports.blockSize]
+   do{ 
+    var goodLoc = true;
+    for (var i = 0; i < exports.players.length; i++){
+      if (distance(goodLoc, exports.players[i].loc) < 50) {
+        goodLoc = false;
+      }
+    }
+    loc = [Math.random()*exports.maxX*exports.blockSize,
+           Math.random()*exports.maxy*exports.blockSize]
+  } while (!goodLoc);
 
+  newEnemy['loc'] = loc;
+  exports.enemies.push(newEnemy);
+};
+
+exports.addStaticObjects = function() {
+  var newStaticObject = {};
+  var loc = [Math.random()*exports.maxX*exports.blockSize,
+             Math.random()*exports.maxy*exports.blockSize]
+   do{ 
+    var goodLoc = true;
+    for (var i = 0; i < exports.staticObjects.length; i++){
+      if (distance(goodLoc, exports.staticObjects[i].loc) < 50) {
+        goodLoc = false;
+      }
+    }
+    loc = [Math.random()*exports.maxX*exports.blockSize,
+           Math.random()*exports.maxy*exports.blockSize]
+  } while (!goodLoc);
+
+  newEnemy['loc'] = loc;
+  exports.staticObjects.push(newStaticObject);
 }
 
-var checkCollisions = function(){
+var checkCollisions = function(enemy, staticObject){
+  // check for box collision between player coords
+  //   and staticObject coords
+  var collided = false;
+  if(enemy.loc[0] > staticObject.loc[0] + exports.blockSize ||
+     enemy.loc[1] > staticObject.loc[1] + exports.blockSize ||
+     staticObject.loc[0] > enemy.loc[0] + exports.blockSize ||
+     staticObject.loc[1] > enemy.loc[1] + exports.blockSize ) {
 
-}
+    collided = true;
+  }
+  return collided;
+};
 
 exports.tickTime = function(){
   //move enemies around and check for collisions
-}
+};
+
