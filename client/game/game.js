31518bb3641b05b079cda63c2d38aa0685361d44
Brian Beeler <BrianBeeler>
2015-07-23 15:08:04 -0500

    View follows Goku

--- client/game/game.js
+++ client/game/game.js
@@ -80,7 +80,7 @@ Game = {
     userID = data.pid;
 
     Crafty.init(Game.width(), Game.height());
-    Crafty.background('url(sprites/landscape.png) fixed' );
+    //Crafty.background('url(sprites/landscape.png) fixed' );
     Crafty.scene('Loading');
   }
 };
