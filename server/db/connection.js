599f21788904e710a016b7988ed444a8d59ad5bc
daniel-jansen <daniel.jansen88@gmail.com>
2015-07-27 14:28:56 -0500

    further postgres heroku development

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
 // connects to the postgres database
 var pg = require('pg');
 var connectionString = "postgres://nflppfgyraiozr:XFaUc5zMPqRLwXwpaAJvGQhKb6@ec2-54-204-3-188.compute-1.amazonaws.com:5432/d1j5k7isn6nt4c"
-var db = new pg.Client();
-db.connect(connectionString);
+var db = new pg.Client(connectionString);
+db.connect();
 exports.db = db;
