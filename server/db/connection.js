64c7b9f10af1929b1b0978c311dd18b039180306
daniel-jansen <daniel.jansen88@gmail.com>
2015-07-27 19:12:11 -0500

    Filled in all required fields

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,13 +1,14 @@
 // connects to the postgres database
 var pg = require('pg');
 var pgData = {
-      host: '',
-      database: '',
-      user : '',
-      port : ,
-      password : '',
+      host: 'ec2-54-204-3-188.compute-1.amazonaws.com',
+      database: 'd1j5k7isn6nt4c',
+      user : 'nflppfgyraiozr',
+      port : 5432,
+      password : 'XFaUc5zMPqRLwXwpaAJvGQhKb6',
       ssl: true
     };
+    
 var db = new pg.Client(process.env.DATABASE_URL || pgData);
 db.connect();
 exports.db = db;
