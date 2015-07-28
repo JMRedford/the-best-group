1596dcc38bffb7c574e592140d7685c67d3b30bc
daniel-jansen <daniel.jansen88@gmail.com>
2015-07-27 19:02:07 -0500

    Set up heroku config vars

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,13 @@
 // connects to the postgres database
 var pg = require('pg');
-var connectionString = "postgres://nflppfgyraiozr:XFaUc5zMPqRLwXwpaAJvGQhKb6@ec2-54-204-3-188.compute-1.amazonaws.com:5432/d1j5k7isn6nt4c"
-var db = new pg.Client(connectionString);
+var pgData = {
+      host: '',
+      database: '',
+      user : '',
+      port : ,
+      password : '',
+      ssl: true
+    };
+var db = new pg.Client(process.env.DATABASE_URL || pgData);
 db.connect();
 exports.db = db;
