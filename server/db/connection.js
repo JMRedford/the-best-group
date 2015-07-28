c41f5a8b99c17f92eb4dc725710df3b9867b8169
John Redford <redford.john.m@gmail.com>
2015-07-27 19:22:48 -0500

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,11 +1,11 @@
 // connects to the postgres database
 var pg = require('pg');
 var pgData = {
-      host: 'ec2-54-204-3-188.compute-1.amazonaws.com',
-      database: 'd1j5k7isn6nt4c',
-      user : 'nflppfgyraiozr',
+      host: 'ec2-107-20-220-251.compute-1.amazonaws.com',
+      database: 'd748t6s12s7k40',
+      user : 'lnjrunoeriehxh',
       port : 5432,
-      password : 'XFaUc5zMPqRLwXwpaAJvGQhKb6',
+      password : 'vcUJT6ord7XXHvNKoHQ2zLlrjW',
       ssl: true
     };
     
